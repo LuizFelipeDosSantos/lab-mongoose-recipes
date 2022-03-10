85d3f13e901615bb1db3f638d2be2387ead6d5eb
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-03-10 14:52:34 +0100

    Return to Promises

--- index.js
+++ index.js
@@ -19,37 +19,25 @@ mongoose
     // Run your code here, after you have insured that the connection was made
 
     //Iteration 2
-    createCarbonara();    
+    const carbonara = new Recipe({
+      title: "Carbonara",
+      level: "Easy Peasy",
+      ingredients: ["eggs", "bacon", "cheese", "pasta"],
+      cuisine: "Italian",
+      dishType: "main_course",
+      duration: 20,
+      creator: "Nicolo" 
+    });
+  
+    carbonara.save();
+    console.log(carbonara.title);    
 
     //Iteration 3
-    insertAllData(); 
+    Recipe.insertMany(data);
 
     //Iteration 4
-    findAndUpdate();
+    Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100});
 })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
-
-async function createCarbonara() {
-  const carbonara = new Recipe({
-    title: "Carbonara",
-    level: "Easy Peasy",
-    ingredients: ["eggs", "bacon", "cheese", "pasta"],
-    cuisine: "Italian",
-    dishType: "main_course",
-    duration: 20,
-    creator: "Nicolo" 
-  });
-
-  await carbonara.save();
-  console.log(carbonara.title);
-}
-
-async function insertAllData() {
-  await Recipe.insertMany(data);
-}
-
-async function findAndUpdate() {
-  await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {$set: {duration: 100}});
-}
